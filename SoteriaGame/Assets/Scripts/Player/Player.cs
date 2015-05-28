4dc20e241338c2a94646e2c3be1955cd4f384459
fiveBit <cuinmanari@gmail.com>
2015-05-28 05:08:55 -0500

    Accidentally comitted wrong files in last upload - fixed.
    -TFIELD

---
+++ SoteriaGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour 
+{
+	public float moveSpeed = 0.15f;
+	public float rotationSpeed = 20.0f;
+	private Animator _animator;
+	public float MoveAngleCorrection = 45.0f;
+	private Vector3 _direction;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this._animator = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this._animator.SetBool("Moving", false);
+		this.ApplyDirection();
+	}
+
+	public void Move()
+	{
+		Vector3 Direction = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		Direction = Quaternion.AngleAxis (this.MoveAngleCorrection, Vector3.up) * Direction;
+	
+		this.ApplyMovement(Direction);
+
+		this._animator.SetBool("Moving", true);
+	}
+	
+	public void ApplyMovement(Vector3 inDirection)
+	{
+		this.transform.rigidbody.velocity = new Vector3(0, 0, 0);
+		this.transform.rigidbody.MovePosition((inDirection * this.moveSpeed) + this.transform.rigidbody.position);
+		this.transform.rigidbody.position = new Vector3( this.transform.rigidbody.position.x, 0.5f, this.transform.rigidbody.rigidbody.position.z);
+
+		this._direction = inDirection;
+	}
+
+	private void ApplyDirection()
+	{
+		if (this._direction != Vector3.zero)
+		{	
+			this.transform.rotation = Quaternion.Slerp(this.transform.rigidbody.rotation, Quaternion.LookRotation(this._direction),Time.deltaTime * 20.0f);
+		}
+	}
+
+}
---
+++ SoteriaGame/Assets/cameraFollowOTS.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollowOTS : MonoBehaviour {
+
+	public Transform target;
+	public float smooth;
+	public Vector3 leftBankCameraOffset;
+	public int crossBridge; // 0 == on left
+							// 1 == on birdge
+							// 2 == on right
+
+	public Quaternion leftBankCameraRotation;
+	public Quaternion bridgeCameraRotation;
+	public Quaternion rightBankCameraRotation;
+
+	public float cameraLocalMovement; // Used to move camera from left to right during transitions.
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		if (target == null)
+			target = GameObject.FindWithTag("Player").transform;
+		crossBridge = 0;
+
+		leftBankCameraOffset = this.transform.position - target.position;
+		leftBankCameraRotation = this.transform.rotation;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(crossBridge == 0)
+			followleftBank();
+		if(crossBridge == 1)
+			followBridge();
+		if(crossBridge == 2)
+			followRightBank();
+
+
+	
+
+	}
+
+	void followleftBank()
+	{
+		this.transform.position = new Vector3(target.position.x + leftBankCameraOffset.x, target.position.y + leftBankCameraOffset.y, target.position.z + leftBankCameraOffset.z);
+		this.transform.rotation = leftBankCameraRotation;
+	}
+
+	void followBridge()
+	{
+		this.transform.position = new Vector3(target.position.x + leftBankCameraOffset.x + cameraLocalMovement, target.position.y + leftBankCameraOffset.y, target.position.z + leftBankCameraOffset.z);
+		this.transform.rotation = bridgeCameraRotation;
+	}
+
+	void followRightBank()
+	{
+		this.transform.position = new Vector3(target.position.x + leftBankCameraOffset.x + (2*cameraLocalMovement), target.position.y + leftBankCameraOffset.y, target.position.z + leftBankCameraOffset.z);
+		//rightBankCameraRotation = this.transform.rotation;
+		this.transform.rotation = rightBankCameraRotation;
+	}
+}
