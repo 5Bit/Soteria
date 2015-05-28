75288f05d83cb4f9daf139ad922858a4e25c8758
fiveBit <cuinmanari@gmail.com>
2015-05-28 04:40:40 -0500

    Second camera programmed - prototyped in TField_test.unity scene. Doesn't modify main camera, save just turning it off. Second camera is standalone, and can be easily modified. Have fun!
    -TFIELD

---
+++ SoteriaGame/Assets/CameraAtCharacter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAtCharacter : MonoBehaviour {
+
+	public GameObject character;
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cam.transform.LookAt(character.transform.position);
+	
+	}
+}
